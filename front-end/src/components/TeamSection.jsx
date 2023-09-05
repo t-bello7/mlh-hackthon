1941107dc752bb17e621b9c080f8424c6e07bdd6
tbello <bello4aus@gmail.com>
2023-09-05 08:57:41 +0100

    feat: add dev images

--- front-end/src/components/TeamSection.jsx
+++ front-end/src/components/TeamSection.jsx
@@ -1,10 +1,24 @@
-import TeamCard from "./molecules/TeamCard";
+import TeamCard from "./molecules/TeamCard"
 import tomiPic from "../assets/imgs/tomi.jpg"
-
+import alexPic from "../assets/imgs/dev-alex.jpg"
+import etornamPic from "../assets/imgs/dev-etornam.jpg"
+import ahmedPic from "../assets/imgs/dev-ahmed.jpg"
 const TeamMembers = [
     {
         name: "Tomi Bello",
         img: tomiPic
+    },
+    {
+        name: "Alex",
+        img: alexPic
+    },
+    {
+        name: "Etornam",
+        img: etornamPic
+    }, 
+    {
+        name: "Ahmed",
+        img: ahmedPic
     }
 ]
 const TeamSection = () => {
